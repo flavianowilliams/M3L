06591653c44bd031320891c0a9873088d3603cfd
flavianowilliams <flavianowilliams@gmail.com>
2022-10-19 21:25:00 -0300

    xxx

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import find_packages
 from numpy.distutils.core import setup, Extension
 
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 ext1 = Extension(name='m3l.dr_module',
                  sources=['src/m3l/dr_module.f95'],
                  f2py_options=['--quiet'],
                  )
 
 setup(
+    install_requires=install_requires,
     package_dir={"":"src"},
     packages=find_packages(where="src"),
     ext_modules=[ext1]
