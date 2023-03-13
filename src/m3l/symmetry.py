7208486ccce1b30c573fe43762c010366f5f80b0
flavianowilliams <flavianowilliams@gmail.com>
2023-03-13 08:42:49 -0300

    git cache cleared

---
+++ src/m3l/symmetry.py
@@ -0,0 +1,34 @@
+from math import exp, sqrt, cos, pi
+from m3l.utils import Constant
+
+class Symm2D(Constant):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.sym_coord = list()
+
+#        self.gFunction()
+
+    def gFunction(self):
+        
+        for atm in self.atoms:
+            sum = 0.e0
+            for atom in self.atoms:
+                if atom['id'] > atm['id']:
+                    dr = sqrt((atm['x']-atom['x'])**2+(atm['y']-atom['y'])**2+(atm['z']-atom['z'])**2)
+                    sum += exp(-self.eta_prm*(dr-self.rs_prm)**2)*self.setSF(dr)
+            atm['s2'] = sum
+            self.sym_coord.append(atm)
+
+        return self.sym_coord
+
+    def setSF(self, dr):
+
+        if dr <= self.rs_prm:
+            func = 0.5*(cos(pi*dr/self.rs_prm)+1.e0)
+        else:
+            func = 0.e0
+
+        return func
