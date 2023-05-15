881ccf6d3549908aec031b7e77205a12fc28cbf3
flavianowilliams <flavianowilliams@gmail.com>
2023-05-15 10:14:27 -0300

    add classe PhysicalConstant no módulo utils

--- src/m3l/utils.py
+++ src/m3l/utils.py
@@ -4,3 +4,8 @@ class Conversion():
 
     aconv = 1.0e10*cte.value('atomic unit of length')
     mconv = 1822.888487e0
+
+class PhsicalConstant():
+
+    kb = cte.value('Boltzmann constant in eV/K')
+        
